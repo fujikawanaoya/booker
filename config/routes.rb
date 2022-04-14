090862d98dcaea5887c3e361429dadc6e102d3e5
EC2 Default User <ec2-user@ip-172-31-41-44.ec2.internal>
2022-04-14 12:57:34 +0000

    エラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'top' => 'homes#top'
+  root to: 'homes#top'
   resources :books
-  post 'books' => 'books#create' 
-  patch 'books/:id' => 'books#update'
+  # post 'books' => 'books#create'
+  # patch 'books/:id' => 'books#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
